79800bc6918f89b1a4b7b4d980ee6035eab19210
EgorBron <bataikinegor@yandex.ru>
2022-06-12 15:35:28 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,51 @@
+"""MIT License
+
+Copyright (c) 2022 EgorBron, Blusutils
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+"""
+
+import disnake
+import os
+import aeval
+from roucore import RoucBot
+from roucore.ext import commands
+from roucore.configuration import ConfigAcceptor
+
+bot = RoucBot(ConfigAcceptor(seed=os.environ['ROUCSEED']))
+
+@bot.listen(event='on_ready')
+async def custom_ready_event():
+    bot.logger.success('Hello world!')
+    swstat = await bot.swish.ping() < 1000
+    dbstat = await bot.get_database_status()
+    bot.logger.debug(f'Swish is {swstat and "working" or "not responding"}. Database is {dbstat and "working" or "not responding"}.')
+
+@bot.command(
+    name = 'eval',
+    aliases = ['evaulate', 'exec', 'execute', 'выполнитькод'], 
+    run_filters = [commands.filters.bot_owners_only], 
+    short_description = bot.new_translatable_text('bot.commands.eval.sdecription'),
+    long_description = bot.new_translatable_text('bot.commands.eval.ldescription'),
+    allow_slash_command = False
+)
+async def __eval(ctx: commands.Context, *, to_eval: str = None):
+    await aeval.aeval(to_eval)
+
+bot.run()
